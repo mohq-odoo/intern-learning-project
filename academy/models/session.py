5503e93bc61cee8b1e5b0565e471b70e9e7a2fcc
Hazique Quazi <mohq@odoo.com>
2022-06-20 18:25:27 -0400

    [ADD] Completed Advanced Views

--- academy/models/session.py
+++ academy/models/session.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from email.policy import default
+
+from urllib3 import encode_multipart_formdata
 from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
+from datetime import timedelta
 
 class Session(models.Model):
 
@@ -22,3 +26,30 @@ class Session(models.Model):
     student_ids = fields.Many2many(comodel_name="res.partner",
         string="Students")
     
+    start_date = fields.Date(string="Start Date", default=fields.Date.today)
+
+    duration = fields.Integer(string="Session Days", default=1)
+
+    end_date = fields.Date(string="End Date",
+        compute='_compute_end_date',
+        inverse='_inverse_end_date',
+        store=True
+    )
+
+    @api.depends('start_date', 'duration')
+    def _compute_end_date(self):
+        for record in self:
+            if not record.start_date and record.duration:
+                record.end_date = record.start_date
+
+            else:
+                duration = timedelta(days=record.duration)
+                record.end_date = record.start_date + duration
+
+    def _inverse_end_date(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                record.duration = (record.end_date - record.start_date).days + 1
+            
+            else:
+                continue
